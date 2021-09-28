eedf03a01ef3564370daa59c9c60036df4cb6514
Igor Gribanov <igor1215@gmail.com>
2021-09-28 17:28:35 -0230

    cz model

--- geometry/cohesivezone.cpp
+++ geometry/cohesivezone.cpp
@@ -20,7 +20,7 @@ void icy::CohesiveZone::AddToSparsityStructure(EquationOfMotionSolver &eq) const
 
 }
 
-bool icy::CohesiveZone::ComputeEquationEntries(EquationOfMotionSolver &eq, const SimParams &prms, double timeStep)
+bool icy::CohesiveZone::ComputeEquationEntries(EquationOfMotionSolver &eq, const SimParams &prms)
 {
 
 }
@@ -28,4 +28,273 @@ bool icy::CohesiveZone::ComputeEquationEntries(EquationOfMotionSolver &eq, const
 void icy::CohesiveZone::AcceptValues()
 {
 
+}
+
+double icy::CohesiveZone::Tn_(const SimParams &prms, const double Dn, const double Dt)
+{
+    const double &deln = prms.cz_del_n;
+    const double &delt = prms.cz_del_t;
+    const double &p_m = prms.cz_p_m;
+    const double &p_n = prms.cz_p_n;
+    const double &alpha = prms.cz_alpha;
+    const double &beta = prms.cz_beta;
+    const double &gam_n = prms.cz_gam_n;
+    const double &gam_t = prms.cz_gam_t;
+    const double &pMtn = prms.cz_pMtn;
+
+    double Dndn = Dn / deln;
+    double Dtdt = Dt / delt;
+    double expr2 = p_m / alpha + Dndn;
+    double pr1 = gam_n / deln;
+    double pr2 = (p_m * pow(1 - Dndn, alpha) * pow(expr2, p_m - 1)) -
+                 (alpha * pow(1 - Dndn, alpha - 1) * pow(expr2, p_m));
+    double pr3 = gam_t * pow(1 - Dtdt, beta) * pow(p_n / beta + Dtdt, p_n) + pMtn;
+    return pr1 * pr2 * pr3;
+}
+
+double icy::CohesiveZone::Tt_(const SimParams &prms, const double Dn, const double Dt)
+{
+    const double &deln = prms.cz_del_n;
+    const double &delt = prms.cz_del_t;
+    const double &p_m = prms.cz_p_m;
+    const double &p_n = prms.cz_p_n;
+    const double &alpha = prms.cz_alpha;
+    const double &beta = prms.cz_beta;
+    const double &gam_n = prms.cz_gam_n;
+    const double &gam_t = prms.cz_gam_t;
+    const double &pMnt = prms.cz_pMnt;
+
+    double Dndn = Dn / deln;
+    double Dtdt = Dt / delt;
+    double expr1 = 1 - Dtdt;
+    double expr2 = p_n / beta + Dtdt;
+    double pr1 = gam_t / delt;
+    double pr2 = p_n * pow(expr1, beta) * pow(expr2, p_n - 1) - beta * pow(expr1, beta - 1) * pow(expr2, p_n);
+    double pr3 = gam_n * pow(1 - Dndn, alpha) * pow(p_m / alpha + Dndn, p_m) + pMnt;
+    return pr1 * pr2 * pr3;
+}
+
+double icy::CohesiveZone::Dnn_(const SimParams &prms, const double opn, const double opt)
+{
+    const double &deln = prms.cz_del_n;
+    const double &delt = prms.cz_del_t;
+    const double &p_m = prms.cz_p_m;
+    const double &p_n = prms.cz_p_n;
+    const double &alpha = prms.cz_alpha;
+    const double &beta = prms.cz_beta;
+    const double &gam_n = prms.cz_gam_n;
+    const double &gam_t = prms.cz_gam_t;
+    const double &pMtn = prms.cz_pMtn;
+
+    double coeff = gam_n / (deln * deln);
+    double expr1 = (p_m * p_m - p_m) * pow(1.0 - (opn / deln), alpha) * pow((p_m / alpha) + (opn / deln), p_m - 2.0);
+    double expr2 = (alpha * alpha - alpha) * pow(1.0 - (opn / deln), alpha - 2.0) * pow((p_m / alpha) + (opn / deln), p_m);
+    double expr3 = 2.0 * alpha * p_m * pow(1.0 - (opn / deln), alpha - 1.0) * pow((p_m / alpha) + (opn / deln), p_m - 1.0);
+    double expr4 = gam_t * pow((1.0 - (opt / delt)), beta) * pow(((p_n / beta) + (opt / delt)), p_n) + pMtn;
+    double result = coeff * (expr1 + expr2 - expr3) * expr4;
+    return result;
+}
+
+double icy::CohesiveZone::Dtt_(const SimParams &prms, const double opn, const double opt)
+{
+    const double &deln = prms.cz_del_n;
+    const double &delt = prms.cz_del_t;
+    const double &p_m = prms.cz_p_m;
+    const double &p_n = prms.cz_p_n;
+    const double &alpha = prms.cz_alpha;
+    const double &beta = prms.cz_beta;
+    const double &gam_n = prms.cz_gam_n;
+    const double &gam_t = prms.cz_gam_t;
+    const double &pMnt = prms.cz_pMnt;
+
+    double coeff = gam_t / (delt * delt);
+    double expr1 = (p_n * p_n - p_n) * pow(1.0 - (opt / delt), beta) * pow((p_n / beta) + (opt / delt), p_n - 2.0);
+    double expr2 = (beta * beta - beta) * pow(1.0 - (opt / delt), beta - 2.0) * pow((p_n / beta) + (opt / delt), p_n);
+    double expr3 = 2.0 * beta * p_n * pow(1.0 - (opt / delt), beta - 1.0) * pow((p_n / beta) + (opt / delt), p_n - 1.0);
+    double expr4 = gam_n * pow(1.0 - (opn / deln), alpha) * pow((p_m / alpha) + (opn / deln), p_m) + pMnt;
+    double result = coeff * (expr1 + expr2 - expr3) * expr4;
+    return result;
+}
+
+double icy::CohesiveZone::Dnt_(const SimParams &prms, const double opn, const double opt)
+{
+    const double &deln = prms.cz_del_n;
+    const double &delt = prms.cz_del_t;
+    const double &p_m = prms.cz_p_m;
+    const double &p_n = prms.cz_p_n;
+    const double &alpha = prms.cz_alpha;
+    const double &beta = prms.cz_beta;
+    const double &gam_n = prms.cz_gam_n;
+    const double &gam_t = prms.cz_gam_t;
+
+    double coeff = gam_n * gam_t / (deln * delt);
+    double expr1 = p_m * pow(1.0 - (opn / deln), alpha) * pow((p_m / alpha) + (opn / deln), p_m - 1.0);
+    double expr2 = alpha * pow(1.0 - (opn / deln), alpha - 1.0) * pow((p_m / alpha) + (opn / deln), p_m);
+    double expr3 = p_n * pow(1.0 - (opt / delt), beta) * pow((p_n / beta) + (opt / delt), p_n - 1.0);
+    double expr4 = beta * pow(1.0 - (opt / delt), beta - 1.0) * pow((p_n / beta) + (opt / delt), p_n);
+    double result = coeff * (expr1 - expr2) * (expr3 - expr4);
+    return result;
+}
+
+void icy::CohesiveZone::PPR_cohesive_zone_formulation(
+    const SimParams &prms,
+    const double opn, const double opt,
+    bool &cz_contact, bool &cz_failed,
+    double &pmax, double &tmax,
+    double &Tn, double &Tt, double &Dnn,
+    double &Dtt, double &Dnt, double &Dtn)
+{
+    const double &deln = prms.cz_del_n;
+    const double &delt = prms.cz_del_t;
+    const double &sigma_max = prms.cz_sigma_max;
+    const double &tau_max = prms.cz_tau_max;
+    const double &lambda_n = prms.cz_lambda_n;
+    const double &lambda_t = prms.cz_lambda_t;
+
+    Tn = Tt = Dnn = Dtt = Dnt = Dtn = 0;
+    if (opn > deln || opt > delt)
+    {
+        cz_contact = false;
+        cz_failed = true;
+        return;
+    }
+    cz_contact = (opn < 0);
+    cz_failed = false;
+    const double threshold_tangential = tau_max * epsilon_fail_traction;
+    const double threshold_normal = sigma_max * epsilon_fail_traction;
+
+    if (cz_contact)
+    {
+        Dnt = 0;
+        if (pmax != 0)
+        {
+            double peakTn = Tn_(prms, pmax, tmax);
+            Tn = peakTn * opn / pmax;
+            Dnn = peakTn / pmax;
+        }
+        else
+        {
+            Dnn = Dnn_(prms, 0, tmax);
+            Tn = Dnn * opn;
+        }
+
+        Tt = Tt_(prms, 0, opt);
+        if (Tt >= epsilon && !(opt > delt * lambda_t / 5 && Tt < threshold_tangential))
+        {
+            if (opt >= tmax)
+            {
+                // tangential softening
+                tmax = opt;
+                Dtt = Dtt_(prms, 0, opt);
+            }
+            else
+            {
+                // unload/reload
+                double peakTt = Tt_(prms, 0, tmax);
+                Tt = peakTt * opt / tmax;
+                Dtt = peakTt / tmax;
+            }
+
+        }
+        else
+        {
+            // cz failed in tangential direction while in contact
+            Tt = Dtt = Dnt = 0;
+            Tn = Dnn = 0;
+            cz_failed = true;
+        }
+    }
+    else
+    {
+        // not in contact
+        Tt = Tt_(prms, opn, opt);
+        Tn = Tn_(prms, opn, opt);
+        if (Tt >= epsilon && Tn >= epsilon &&
+            !(opt > delt * lambda_t / 5 && Tt < threshold_tangential) &&
+            !(opn > deln * lambda_n / 5 && Tn < threshold_normal))
+        {
+            // tangential component
+            bool tsoft = (opt >= tmax);
+            bool nsoft = (opn >= pmax);
+            if (tsoft && nsoft)
+            {
+                // tangential and normal softening
+                tmax = opt;
+                pmax = opn;
+                Dnn = Dnn_(prms, opn, opt);
+                Dnt = Dnt_(prms, opn, opt);
+                Dtt = Dtt_(prms, opn, opt);
+            }
+            else if (tsoft && !nsoft)
+            {
+                Dnt = 0;
+                if (pmax != 0)
+                {
+                    double peakTn = Tn_(prms, pmax, tmax);
+                    Tn = peakTn * opn / pmax;
+                    Dnn = peakTn / pmax;
+                }
+                else
+                {
+                    Tn = 0; Dnn = Dnn_(prms, 0, tmax);
+                }
+
+                // normal unload/reload
+                tmax = opt;
+                Tt = Tt_(prms, pmax, opt);
+                Dtt = Dtt_(prms, pmax, opt);
+            }
+            else if (!tsoft && nsoft)
+            {
+                Dnt = 0;
+                if (tmax != 0)
+                {
+                    double peakTt = Tt_(prms, pmax, tmax);
+                    Tt = peakTt * opt / tmax;
+                    Dtt = peakTt / tmax;
+                }
+                else
+                {
+                    Tt = 0; Dtt = Dtt_(prms, pmax, 0);
+                }
+
+                pmax = opn;
+                Tn = Tn_(prms, pmax, tmax);
+                Dnn = Dnn_(prms, pmax, tmax);
+
+            }
+            else
+            {
+                Dnt = 0;
+                // reloading in both tangential and normal
+                double peakTn = Tn_(prms, pmax, tmax);
+                if (pmax != 0)
+                {
+                    Tn = peakTn * opn / pmax;
+                    Dnn = peakTn / pmax;
+                }
+                else
+                {
+                    Tn = 0; Dnn = Dnn_(prms, 0, tmax);
+                }
+
+                if (tmax != 0)
+                {
+                    double peakTt = Tt_(prms, pmax, tmax);
+                    Tt = peakTt * opt / tmax;
+                    Dtt = peakTt / tmax;
+                }
+                else
+                {
+                    Tt = 0; Dtt = Dtt_(prms, pmax, 0);
+                }
+            }
+        }
+        else
+        {
+            cz_failed = true;
+            Tn = Tt = Dnn = Dtt = Dnt = 0;
+        }
+    }
+    Dtn = Dnt;
 }
--- geometry/cohesivezone.h
+++ geometry/cohesivezone.h
@@ -1,3 +1,8 @@
+// Park, Kyoungsoo, and Glaucio H. Paulino. "Computational implementation of
+// the PPR potential-based cohesive model in ABAQUS: Educational perspective."
+// Engineering fracture mechanics 93 (2012): 239-262.
+
+
 #ifndef COHESIVEZONE_H
 #define COHESIVEZONE_H
 
@@ -11,7 +16,7 @@ namespace icy {struct CohesiveZone; struct Node; struct Element; }
 
 struct icy::CohesiveZone
 {
-    Node *nds[4];           // two nodes per side
+    Node *nds[4];           // two nodes per side: Side_A_Node_1, Side_A_Node_2, Side_B_Node_1, Side_B_Node_2
     Element *elems[2];      // each CZ connects two elements
     double pmax[2];         // max normal separation reached
     double tmax[2];         // max tangential separation reached
@@ -22,14 +27,32 @@ struct icy::CohesiveZone
     void Reset();
     void Initialize(Node *nd1a, Node *nd2a, Node *nd1b, Node *nd2b);
     void AddToSparsityStructure(EquationOfMotionSolver &eq) const;
-    bool ComputeEquationEntries(EquationOfMotionSolver &eq, const SimParams &prms, double timeStep);
+    bool ComputeEquationEntries(EquationOfMotionSolver &eq, const SimParams &prms);
     void AcceptValues();
 
 private:
     bool tentative_contact, tentative_failed, tentative_damaged;
     double tentative_avgDn, tentative_avgDt, tentative_avgTn, tentative_avgTt;
-    double tentative_pmax, tentative_tmax;
+    double tentative_pmax_final, tentative_tmax_final;
     double tentative_pmax[2], tentative_tmax[2];
+
+    constexpr static double epsilon = -1e-9;
+    constexpr static double epsilon_fail_traction = 0.05; // if traction is <5% of max, CZ fails
+
+
+    static double Tn_(const SimParams &prms, const double Dn, const double Dt);
+    static double Tt_(const SimParams &prms, const double Dn, const double Dt);
+    static double Dnn_(const SimParams &prms, const double opn, const double opt);
+    static double Dtt_(const SimParams &prms, const double opn, const double opt);
+    static double Dnt_(const SimParams &prms, const double opn, const double opt);
+
+    static void PPR_cohesive_zone_formulation(
+        const SimParams &prms,
+        const double opn, const double opt,
+        bool &cz_contact, bool &cz_failed,
+        double &pmax, double &tmax,
+        double &Tn, double &Tt, double &Dnn,
+        double &Dtt, double &Dnt, double &Dtn);
 
 };
 
